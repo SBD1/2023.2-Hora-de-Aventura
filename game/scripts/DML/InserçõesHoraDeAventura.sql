730cc3e497f594de25a58255c054b91dd3d263eb
FilhoDoan <doangalvaofilho@gmail.com>
2023-11-17 15:48:56 -0300

    Adicionando arquitetura do jogo

---
+++ game/scripts/DML/InserçõesHoraDeAventura.sql
@@ -0,0 +1,47 @@
+insert into mundo values('Mundo de O','Cubo do prismo');
+
+insert into regiao values('Reino Doce', 'Mundo de O');
+
+insert into local values('1','Casa onde habita o canelinha' ,'Casa do canelinha', false, 'Reino Doce');
+ 
+insert into personagem values('1',false);
+
+insert into npc values('1','Litch',1000,false, 100,'Primordial', 1000, 1000);
+
+insert into instancia values(1,1 ,1000 , 0001);
+
+insert into missao values('Salvar princesa ',true , 'Salvar princesa',
+100);
+
+insert into chefe values (1,'Salvar princesa ',1);
+
+insert into personagem  values('2', true);
+
+insert into pc values('2', 'Finn', 50, 40, 40, 0, 'humano', 40, 20, 1);
+
+insert into fazmissao values(2, 'Salvar princesa', false);  
+
+insert into contem values(1,'Salvar princesa', 'Incompleto' );
+
+insert into local values('0002','Casa onde habita Finn' ,'Casa na árvore', false, 'Reino Doce');
+
+insert into habilidade values(1, 'Elasticidade', 0, 100, 10);
+
+insert into possuihab values('2',1);
+
+insert into item values('001','Espada de Grama', 20, 100, 'Ferramenta' );
+
+insert into inventario values(1,1,2);
+
+insert  into instanciaitem values(1, 1, 100, 1); 
+
+insert into dropa values(1,1,0.22);
+
+insert into loja values('Loja de Arma', 'Arma','Espadas', 2 );
+
+insert into possuiitem values(1,'Loja de Arma', 'Espadas', 2);
+
+
+
+
+
