917e1f17c16f0640a6607168f9c0f5564122c988
HeBatalha <henriqueabatalha@gmail.com>
2023-11-23 20:57:19 -0300

    #11: Adicionando Trigger Generalização Personagem

--- game/scripts/DDL/GeneralizacaoPersonagem.sql
+++ game/scripts/DDL/GeneralizacaoPersonagem.sql
@@ -13,3 +13,19 @@ CREATE TRIGGER check_pc -- Trigger para adicionar NPC
 BEFORE UPDATE OR INSERT ON npc
 FOR EACH ROW
 EXECUTE PROCEDURE check_pc();
+
+CREATE OR REPLACE FUNCTION check_npc() RETURNS trigger AS -- Procedure para checar se o item adicionado ja pertence a npc
+    $check_npc$
+        BEGIN 
+            PERFORM * FROM npc WHERE Personagem = NEW.Personagem;
+            IF FOUND THEN
+                RAISE EXCEPTION 'O Id ja pertence a npc';
+            END IF;
+            RETURN NEW;
+        END
+    $check_npc$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_npc -- Trigger para adicionar PC
+BEFORE UPDATE OR INSERT ON pc
+FOR EACH ROW
+EXECUTE PROCEDURE check_npc();
