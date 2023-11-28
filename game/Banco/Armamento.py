7324ea2820d2bc32e9d9ccd245714beebcfa76ac
Dyetrix <arthur.lantr@gmail.com>
2023-11-28 17:42:06 -0300

    Classe Armamento e operações básicas do banco

--- game/Banco/Armamento.py
+++ game/Banco/Armamento.py
@@ -1,25 +1,26 @@
 import psycopg2
 from Database import Database
 
-class Item:
-    
-    def __init__(self, idItem: int, atcItem: int):
-        self.idItem = idItem
-        self.atcItem = atcItem
+class Armamento:
+    def __init__(self, item: int, nome: str, dano: int, elemento: str):
+        self.item = item
+        self.nome = nome
+        self.dano = dano
+        self.elemento = elemento
     pass
 
     def __init__(self):
         self.db = Database()
     pass
 
-    def inserirItem(self, idItem: int, atcItem: int):
+    def inserirArmamento(self, item: int, nome: str, dano: int, elemento: str):
         try:
             conexao = self.db.conexao
             cursor = conexao.cursor()
 
-            cursor.execute("INSERT INTO item VALUES(%s, %s);", (idItem, atcItem))
-            insercaoFazMissao = conexao.commit()
-            return print("Item inserido com sucesso!\n")
+            cursor.execute("INSERT INTO armamento VALUES(%s, %s, %s, %s);", (item, nome, dano, elemento))
+            insercaoArmamento = conexao.commit()
+            return print("Arma inserida com sucesso!\n")
 
         except psycopg2.IntegrityError as e:
             print(f"Encontramos problemas ao fazer a insercao. Erro: {e}\n")
@@ -27,14 +28,14 @@ class Item:
         finally:
             cursor.close()
 
-    def deletarItem(self, idItem: int):
+    def deletarArmamento(self, item: int):
         try:
             conexao = self.db.conexao
             cursor =  conexao.cursor()
 
-            cursor.execute(f"DELETE FROM item WHERE iditem = '{idItem}';")
-            delecaoFazMissao = conexao.commit()
-            return print("Item deletado com sucesso!\n")
+            cursor.execute(f"DELETE FROM armamento WHERE item = '{item}';")
+            delecaoArmamento = conexao.commit()
+            return print("Arma deletada com sucesso!\n")
         
         except psycopg2.IntegrityError as e:
             print(f"Encontramos problemas ao fazer a delecao. Erro: {e}\n")
@@ -42,15 +43,15 @@ class Item:
         finally:
             cursor.close()
 
-    def consultarItemId(self, idItem: int):
+    def consultarArmamentoId(self, item: int):
         try:
             conexao = self.db.conexao
             cursor = conexao.cursor()
-            cursor.execute(f"SELECT * FROM item WHERE iditem = '{idItem}';")
+            cursor.execute(f"SELECT * FROM armamento WHERE item = '{item}';")
             consulta = cursor.fetchall()
 
             if not consulta:
-                print("Não foi há itens cadastrados\n")
+                print("Não foi há armas cadastradas\n")
             else:
                 for i in consulta:
                     print(i)
@@ -62,15 +63,15 @@ class Item:
         finally:
             cursor.close()
 
-    def consultarItem(self):
+    def consultarArmamento(self):
         try:
             conexao = self.db.conexao
             cursor = conexao.cursor()
-            cursor.execute(f"SELECT * FROM item;")
+            cursor.execute(f"SELECT * FROM armamento;")
             consulta = cursor.fetchall()
 
             if not consulta:
-                print("Não foi há itens cadastrados\n")
+                print("Não foi há armas cadastradas\n")
             else:
                 for i in consulta:
                     print(i)
@@ -82,3 +83,5 @@ class Item:
         finally:
             cursor.close()
 
+a = Armamento()
+a.consultarArmamentoId(1)
