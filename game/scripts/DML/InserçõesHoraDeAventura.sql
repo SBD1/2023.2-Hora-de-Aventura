90cf9ceb05f21fc8c5c966e9ae0a49c23220d113
Davi Nobre <101183717+Jagaima@users.noreply.github.com>
2023-12-02 17:49:24 -0300

    Inserindo alguns locais novos

--- game/scripts/DML/InserçõesHoraDeAventura.sql
+++ game/scripts/DML/InserçõesHoraDeAventura.sql
@@ -1,9 +1,139 @@
+insert into mundo values('Cubo do Prismo','Cubo do prismo');
+
 insert into mundo values('Mundo de O','Cubo do prismo');
+
+insert into mundo values('Noitosfera','Mundo de O');
+
+insert into mundo values('Espaço','Mundo de O');
+
+
 
 insert into regiao values('Reino Doce', 'Mundo de O');
 
-insert into local values('1','Casa onde habita o canelinha' ,'Casa do canelinha', false, 'Reino Doce');
+insert into regiao values('Planice', 'Mundo de O');
+
+insert into regiao values('WaterWorld', 'Mundo de O');
+
+insert into regiao values('Selva', 'Mundo de O');
+
+insert into regiao values('Reino Gelado', 'Mundo de O');
+
+insert into regiao values('Deserto', 'Mundo de O');
+
+insert into regiao values('Pantano', 'Mundo de O');
+
+insert into regiao values('Reino de Fogo', 'Mundo de O');
+
+insert into regiao values('Céu', 'Mundo de O');
+
+insert into regiao values('Reino Caroço', 'Mundo de O');
+
+insert into regiao values('Exosfera', 'Espaço');
+
+insert into regiao values('Inferno', 'Noitosfera');
+
+
+
+
+insert into local values('0','Foi aqui que tudo começou!' ,'Início', false, 'Reino Doce');
+
+insert into local values('1','Estes portões guardam o castelo da princesa Jujuba' ,'Portão Castelo Doce', false, 'Reino Doce');
+
+insert into local values('2','Casa onde habita o canelinha' ,'Casa do canelinha', false, 'Reino Doce');
+
+insert into local values('3','Aqui você vai encontrar armas importantes pra começar sua jornada' ,'Loja do Doan', false, 'Reino Doce');
+
+insert into local values('4','As gramas brancas de maria mole aos poucos dão lugar a grama de verdade' ,'Planices Doces', false, 'Reino Doce');
+
+insert into local values('5','Aqui tem grama até onde a vista alcança' ,'Planices Verdes', true, 'Planice');
+
+insert into local values('6','Uma luz mística emana no fundo do laguinho' ,'Laguinho', true, 'Planice');
+
+insert into local values('7','Luzes coloridas passam por você, aqui parece que você se encontra entre dois mundos' ,'Portal do Laguinho', false, 'Planice');
+
+insert into local values('8','Eca é esgoto!' ,'Esgoto', true, 'WaterWorld');
+
+insert into local values('9','Susana forte, uma moça muito grande e forte mora aqui. Sua casa é muito fofa' ,'Casa da Susana Forte', false, 'WaterWorld');
+
+insert into local values('10','Aqui vive um povo peixe que parece humano' ,'Cidade subterranea', false, 'WaterWorld');
+
+insert into local values('16','Jardins da princesa Jujuba' ,'Jardins doce', false, 'Reino Doce');
+
+insert into local values('19','Aqui você vai encontrar armas importantes pra começar sua jornada' ,'Loja do Arthur', false, 'Planice');
+
+insert into local values('20','Aqui mora o Jake e seu amigo Finn' ,'Casa na árvore', false, 'Planice');
+
+insert into local values('23','Aqui morava o toucinho do mar, está abandonado' ,'Casa do Toucinho', true, 'WaterWorld');
+
+insert into local values('24','Quase não dá pra ver algo aqui. Como estamos respirando?' ,'Profundezas', true, 'WaterWorld');
+
+insert into local values('25','A água aqui é muito quente e é um local muito grande' ,'Toca do monstro', true, 'WaterWorld');
+
+insert into local values('26','Um vulcão se encontra em um buraco em baixo da agua' ,'Vulcão submerso', true, 'WaterWorld');
+
+insert into local values('27','Uma praia de águas mornas. Que maravilha' ,'Prainha', false, 'Reino de Fogo');
+
+insert into local values('28','O malvado rei de fogo prende sua filha aqui' ,'Sela da princesa', false, 'Reino de Fogo');
+
+insert into local values('29','Dói pisar aqui' ,'Areia quente', false, 'Reino de Fogo');
+
+insert into local values('30','Aqui a princesa jujuba dorme' ,'Quarto da Jujuba', false, 'Reino Doce');
+
+insert into local values('31','Aqui é o lobby' ,'Salão castelo doce', false, 'Reino Doce');
+
+insert into local values('33','Um parquinho onde o BMO brinca' ,'Parquinho', true, 'Planice');
+
+insert into local values('34','Tem algumas árvores antigas aqui' ,'Arvoreado', true, 'Planice');
+
+insert into local values('35','Tem muitas árvores antigas aqui' ,'Bosque', true, 'Planice');
+
+insert into local values('36','Tem velharias e cheiro de poeira aqui, Nada de muito útil' ,'Depósito velho', false, 'Planice');
+
+insert into local values('43','O mais puro piso de obsidiana. O rei de fogo mora aqui' ,'Sala do trono', true, 'Reino de Fogo');
+
+insert into local values('45','Muitos bandidos são presos aqui' ,'calabouço', true, 'Reino Doce');
+
+insert into local values('46','Aqui a princesa fabrica seu povo. Literalmente' ,'Laboratório', false, 'Reino Doce');
+
+insert into local values('49','Aqui a mata é muito densa e traiçoeira' ,'Inicio da Selva', true, 'Selva');
+
+insert into local values('50','A floresta aqui uiva como um lobo' ,'Floresta uivante', true, 'Selva');
+
+insert into local values('51','Aqui começa a neve do reino gelado' ,'Entrada do gelo', true, 'Reino Gelado');
+
+insert into local values('52','Aqui é muito frio' ,'Salão do rei gelado', true, 'Reino Gelado');
+
+insert into local values('53','Rei gelado dorme aqui' ,'Quarto do rei gelado', false, 'Reino Gelado');
+
+insert into local values('54','aqui tem muitos brinquedos' ,'Quarto do gunter', false, 'Reino Gelado');
+
+insert into local values('55','aqui o rei gelado tortura as princesas com sua voz' ,'Karâoke', false, 'Reino Gelado');
+
+insert into local values('57','aqui tem flores que parecem velas' ,'Jardim de velas', true, 'Reino de Fogo');
+
+insert into local values('58','Os portões vermelhos de calor aquecem até sua alma' ,'Portões flamejantes', true, 'Reino de Fogo');
+
+insert into local values('59','aqui tem flores que parecem luminárias' ,'Jardim de lumos', true, 'Reino de Fogo');
+
+insert into local values('63','aqui mora a belíssima maga caçadora' ,'porta da maga', false, 'Selva');
+
+insert into local values('64','aqui o cheiro de carniça das vítimas é tanto que é tonteante' ,'floresta dos perigos', true, 'Selva');
+
+insert into local values('65','A floresta aqui uiva ainda mais' ,'floresta UIVADORA', true, 'Selva');
+
+insert into local values('66','Tem muita lama aqui, é até difícil andar' ,'floresta pantanosa', true, 'Pantano');
+
+insert into local values('68','Muitas princesas já passaram por aqui' ,'Calabouço gelado', false, 'Reino Gelado');
+
+insert into local values('69','Muitos cadernos preenchem as prateleiras, repletos de histórias de Fiona e Cake, escritas pelo rei gelado' ,'Biblioteca', false, 'Reino Gelado');
+
+insert into local values('73','Ao invés de copas de folhas, as daqui tem cópas de fogo e metal derretido' ,'Floresta Forno', true, 'Reino de Fogo');
+
+insert into local values('76','Aqui a maga da floresta ora para seus deuses' ,'totem de oração', false, 'selva');
  
+
+
+
 insert into personagem values('1',false);
 
 insert into npc values('1','Litch',1000,false, 100,'Primordial', 1000, 1000);
