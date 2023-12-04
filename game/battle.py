cc663bf3c8933fcc3496daad15e5b80123b6b66f
Dyetrix <arthur.lantr@gmail.com>
2023-12-04 12:55:21 -0300

    Trigger para instancia morta e polindo o jogo

--- game/battle.py
+++ game/battle.py
@@ -1,5 +1,18 @@
 from Banco import *
 from random import randint
+import sys, time, os
+
+def print_devagar(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.041)
+
+def print_um_pouco_mais_rapido(str):
+    for letter in str:
+        sys.stdout.write(letter)
+        sys.stdout.flush()
+        time.sleep(0.005)
 
 def luta(idNPC: int, numInstancia: int, jogadorID: int):
    
@@ -8,6 +21,7 @@ def luta(idNPC: int, numInstancia: int, jogadorID: int):
         inimigo = Npc() #atributos do monstro que estao na tabela npc
 
         i = inimigo.getNPCID(idNPC) # Pegando a informacao da tupla do monstro passado pela funcao
+        
         
         #carregando status vindos da tabela npc
         nome = i[0][1] 
@@ -19,6 +33,9 @@ def luta(idNPC: int, numInstancia: int, jogadorID: int):
         #pegando a vida da instancia
         inimigoAux = Instancia() #atributos da tabela instancia, especificando qual monstro que é
         iAux = inimigoAux.getInstanciaID(idNPC,numInstancia) # Acessando a informacao da instancia especifica do monstro
+        if iAux is None:
+            print_devagar("\nInimigo já foi derrotado ou não existe\n")
+            break
         vidaInstacia = iAux[0][2]
 
         if(vidaInstacia <= 0):
@@ -35,25 +52,25 @@ def luta(idNPC: int, numInstancia: int, jogadorID: int):
 
 
 
-        print("\033[1;32m\n\n                          ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   \033[0m")
-        print("\033[1;32m                       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀          \033[0m")
-        print("\033[1;32m                      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣽⣦⣀⡀⠤⠤⣤⣀⡀⠀⠀⠀⠀⠀          \033[0m")
-        print("\033[1;34m                       ⠀⠀⠀⠀⣀⠴⠊⡉⠔⢛⠭⠉⡇⠙⢄⠈⠐⠮⡉⠒⠬⣑⠢⣀⠀⠀       \033[0m")
-        print("\033[1;34m                       ⠀⠀⣠⠞⠁⡴⠋⠀⡰⠊⡀⠀⢸⠀⠀⢣⠀⠀⣦⢄⡀⠈⠣⡈⢧⡀          \033[0m ")
-        print("\033[1;34m                       ⠀⡔⠁⢀⠎⠀⠀⡰⢡⢮⣇⠀⢸⠀⠀⠀⠃⠀⣿⣦⡙⡄⠀⢷⡀⢃          \033[0m ")
-        print("\033[1;34m                       ⠸⠁⠀⡞⠀⠀⢀⣇⡇⣾⣿⣆⠀⠀⠀⠀⡸⠘⠛⠛⠛⡉ ⠀⠀⡇⢸         \033[0m ")
-        print("\033[1;34m                       ⡆⠀⢸⠀⠀⠀⢸⠉⠉⠉⠉⠁⠀⠀⢻⣿⢹⠀⠀⠀⠀⣠  ⠀⠀⠁⢸        \033[0m  ")
-        print("\033[1;34m                       ⡇⠀⢸⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⡀⠀⠫⢾⠀⢀⣤⢞⠋⠀  ⢠⠃⠸        \033[0m  ")
-        print("\033[1;34m                       ⢡⠀⠈⠀⠀⠀⠈⢳⡦⣄⣀⣤⣀⣧⣶⣤⣾⣿⣿⣣ ⢫⠀⠀⡏⢠⠆         \033[0m ")
-        print("\033[1;34m                       ⠈⢧⡀⠑⡀⠀⠀⠘⠝⠾⠿⣿⣿⡿⠿⡿⠛⠏⠉⠀⡜⢠⠞⣠⠏⠀          \033[0m ")
-        print("\033[1;34m                       ⠀⠀⠳⣄⠈⢤⠀⠀⠈⢢⡀⠈⠋⡇⠀⠀⠸⠀⠀⡔⡠⣋⡴⠋⠀           \033[0m  ")
-        print("\033[1;34m                        ⠀⠀⠀⠈⠙⠲⠯⠶⢤⣀⣑⣦⣀⡇⠤⠴⠥⠴⠚⠈⠉⠁⠀⠀⠀⠀         \033[0m ")
+        print_um_pouco_mais_rapido("\033[1;32m\n\n                          ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⡷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀   \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;32m                       ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀          \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;32m                      ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣽⣦⣀⡀⠤⠤⣤⣀⡀⠀⠀⠀⠀⠀          \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⠀⠀⠀⠀⣀⠴⠊⡉⠔⢛⠭⠉⡇⠙⢄⠈⠐⠮⡉⠒⠬⣑⠢⣀⠀⠀       \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⠀⠀⣠⠞⠁⡴⠋⠀⡰⠊⡀⠀⢸⠀⠀⢣⠀⠀⣦⢄⡀⠈⠣⡈⢧⡀          \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⠀⡔⠁⢀⠎⠀⠀⡰⢡⢮⣇⠀⢸⠀⠀⠀⠃⠀⣿⣦⡙⡄⠀⢷⡀⢃          \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⠸⠁⠀⡞⠀⠀⢀⣇⡇⣾⣿⣆⠀⠀⠀⠀⡸⠘⠛⠛⠛⡉ ⠀⠀⡇⢸         \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⡆⠀⢸⠀⠀⠀⢸⠉⠉⠉⠉⠁⠀⠀⢻⣿⢹⠀⠀⠀⠀⣠  ⠀⠀⠁⢸        \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⡇⠀⢸⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⡀⠀⠫⢾⠀⢀⣤⢞⠋⠀  ⢠⠃⠸        \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⢡⠀⠈⠀⠀⠀⠈⢳⡦⣄⣀⣤⣀⣧⣶⣤⣾⣿⣿⣣ ⢫⠀⠀⡏⢠⠆         \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⠈⢧⡀⠑⡀⠀⠀⠘⠝⠾⠿⣿⣿⡿⠿⡿⠛⠏⠉⠀⡜⢠⠞⣠⠏⠀          \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                       ⠀⠀⠳⣄⠈⢤⠀⠀⠈⢢⡀⠈⠋⡇⠀⠀⠸⠀⠀⡔⡠⣋⡴⠋⠀           \033[0m\n")
+        print_um_pouco_mais_rapido("\033[1;34m                        ⠀⠀⠀⠈⠙⠲⠯⠶⢤⣀⣑⣦⣀⡇⠤⠴⠥⠴⠚⠈⠉⠁⠀⠀⠀⠀         \033[0m\n")
 
-        print(f"Inimigo = {nome}\nEspecie = {especie}\nNivel = {nivel}\nVida = {vidaInstacia}\n\n")
-        print(f"{nomeJogador} | vida = {vidaJogador} | lvl = {nivelJogador} | dano = {forcaJogador} | defesa = {defesaJogador}\n\n")
+        print_devagar(f"\033[1;35mInimigo = \033[0m{nome}\n\033[1;35mEspecie = \033[0m{especie}\n\033[1;35mNivel = \033[0m{nivel}\n\033[1;35mVida = \033[0m{vidaInstacia}\n\n")
+        print_devagar(f"{nomeJogador} | vida = {vidaJogador} | lvl = {nivelJogador} | dano = {forcaJogador} | defesa = {defesaJogador}\n\n")
 
-
-        print("atacar/fugir?\n\n")
+        
+        print_devagar("atacar/fugir?\n\n")
         opcao = input()
             
         match opcao:
@@ -62,17 +79,17 @@ def luta(idNPC: int, numInstancia: int, jogadorID: int):
             case 'atacar':
                 
                 #listar opcoes de ataque que o personagem possui: habilidades e itens do inventario que são do tipo armamento
-                print("Voce pode atacar com habilidades, itens ou usar poção: (hab/it/poc)\n")
+                print_devagar("Voce pode atacar com habilidades, itens ou usar poção: (hab/it/poc)\n\n")
                 atacar = input()
 
                 if(atacar == "habilidade" or atacar == "hab" or atacar == "habilidades"):
-                    print("habilidades disponiveis:\n")
-                    print("ID | Nome | Cooldown | Dano\n\n")
+                    print_devagar("\nhabilidades disponiveis:\n")
+                    print_devagar("ID | Nome | Cooldown | Dano\n\n")
                     
                     consulta = PossuiHab()
                     consulta.consultarPossuiHabPersonagem(jogadorID)
 
-                    print("\nDigite apenas o id da habilidade!\n")   
+                    print_devagar("\nDigite apenas o id da habilidade!\n")   
 
                     hab = input()
                     ID = consulta.getPossuiHabPK(jogadorID, hab)
@@ -86,38 +103,78 @@ def luta(idNPC: int, numInstancia: int, jogadorID: int):
                         danoInfligidoJogador = danoMonstro - defesaJogador
                         danoInfligidoMonstro = danoAtaque - defesaMonstro
 
-                        print(f"jogador -{danoInfligidoJogador}")
-                        print(f"monstro -{danoInfligidoMonstro}")
+                        os.system('cls')
+                        print_devagar(f"jogador -{danoInfligidoJogador}\n")
+                        print_devagar(f"monstro -{danoInfligidoMonstro}")
 
                         if(danoInfligidoJogador > 0):
                             novaVida = vidaJogador - danoInfligidoJogador
                             jogador.atualizarVidaPCID(jogadorID, novaVida)
+                            if(novaVida <= 0):
+                                print_um_pouco_mais_rapido("\033[0;31m   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⠀⢀⣴⣿⡶⠀⣾⣿⣿⡿⠟⠛⠁\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⣶⣶⣦⠀⠀⠀⠀⣼⣿⣿⡇⠀⣠⣿⣿⣿⠇⣸⣿⣿⣧⣤⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⢀⣴⣾⣿⡿⠿⠿⠿⠇⠀⠀⣸⣿⣿⣿⡆⠀⠀⢰⣿⣿⣿⣷⣼⣿⣿⣿⡿⢀⣿⣿⡿⠟⠛⠁⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⣴⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⢠⣿⣿⣹⣿⣿⣿⣿⣿⣿⡏⢻⣿⣿⢿⣿⣿⠃⣼⣿⣯⣤⣴⣶⣿⡤⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣼⣿⠏⠀⣀⣠⣤⣶⣾⣷⠄⣰⣿⣿⡿⠿⠻⣿⣯⣸⣿⡿⠀⠀⠀⠁⣾⣿⡏⢠⣿⣿⠿⠛⠋⠉⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣿⣿⠲⢿⣿⣿⣿⣿⡿⠋⢰⣿⣿⠋⠀⠀⠀⢻⣿⣿⣿⠇⠀⠀⠀⠀⠙⠛⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠹⢿⣷⣶⣿⣿⠿⠋⠀⠀⠈⠙⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣴⣶⣦⣤⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⡇⢰⣶⣶⣾⡿⠷⣿⣿⣿⡟⠛⣉⣿⣿⣿⠆\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⡎⣿⣿⣦⠀⠀⠀⢀⣤⣾⠟⢀⣿⣿⡟⣁⠀⠀⣸⣿⣿⣤⣾⣿⡿⠛⠁⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⣠⣾⣿⡿⠛⠉⢿⣦⠘⣿⣿⡆⠀⢠⣾⣿⠋⠀⣼⣿⣿⣿⠿⠷⢠⣿⣿⣿⠿⢻⣿⣧⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣴⣿⣿⠋⠀⠀⠀⢸⣿⣇⢹⣿⣷⣰⣿⣿⠃⠀⢠⣿⣿⢃⣀⣤⣤⣾⣿⡟⠀⠀⠀⢻⣿⣆⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣿⣿⡇⠀⠀⢀⣴⣿⣿⡟⠀⣿⣿⣿⣿⠃⠀⠀⣾⣿⣿⡿⠿⠛⢛⣿⡟⠀⠀⠀⠀⠀⠻⠿⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠹⣿⣿⣶⣾⣿⣿⣿⠟⠁⠀⠸⢿⣿⠇⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠁\033[0m\n")
+                                break
                         
                         if(danoInfligidoMonstro > 0):
                             novaVidaMonstro = vidaInstacia - danoInfligidoMonstro
                             inimigoAux.atualizarVidaInstanciaID(idNPC, numInstancia, novaVidaMonstro)
                     
                     else:
+                        os.system('cls')
+                        print_devagar("\nHabilidade inexistente\n")
+                        
                         danoMonstro = forca + randint(-4,3)
                         defesa = defesaJogador + randint(-4,3)
                         danoInfligidoJogador = danoMonstro - defesaJogador
 
-                        print(f"jogador{danoInfligidoJogador}\n")
+                        print_devagar(f"jogador -{danoInfligidoJogador}\n")
 
                         if(danoInfligidoJogador > 0):
                             novaVida = vidaJogador - danoInfligidoJogador
                             jogador.atualizarVidaPCID(jogadorID, novaVida)
+                            if(novaVida <= 0):
+                                print_um_pouco_mais_rapido("\033[0;31m   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⠀⢀⣴⣿⡶⠀⣾⣿⣿⡿⠟⠛⠁\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⣶⣶⣦⠀⠀⠀⠀⣼⣿⣿⡇⠀⣠⣿⣿⣿⠇⣸⣿⣿⣧⣤⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⢀⣴⣾⣿⡿⠿⠿⠿⠇⠀⠀⣸⣿⣿⣿⡆⠀⠀⢰⣿⣿⣿⣷⣼⣿⣿⣿⡿⢀⣿⣿⡿⠟⠛⠁⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⣴⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⢠⣿⣿⣹⣿⣿⣿⣿⣿⣿⡏⢻⣿⣿⢿⣿⣿⠃⣼⣿⣯⣤⣴⣶⣿⡤⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣼⣿⠏⠀⣀⣠⣤⣶⣾⣷⠄⣰⣿⣿⡿⠿⠻⣿⣯⣸⣿⡿⠀⠀⠀⠁⣾⣿⡏⢠⣿⣿⠿⠛⠋⠉⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣿⣿⠲⢿⣿⣿⣿⣿⡿⠋⢰⣿⣿⠋⠀⠀⠀⢻⣿⣿⣿⠇⠀⠀⠀⠀⠙⠛⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠹⢿⣷⣶⣿⣿⠿⠋⠀⠀⠈⠙⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣴⣶⣦⣤⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⡇⢰⣶⣶⣾⡿⠷⣿⣿⣿⡟⠛⣉⣿⣿⣿⠆\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⡎⣿⣿⣦⠀⠀⠀⢀⣤⣾⠟⢀⣿⣿⡟⣁⠀⠀⣸⣿⣿⣤⣾⣿⡿⠛⠁⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⣠⣾⣿⡿⠛⠉⢿⣦⠘⣿⣿⡆⠀⢠⣾⣿⠋⠀⣼⣿⣿⣿⠿⠷⢠⣿⣿⣿⠿⢻⣿⣧⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣴⣿⣿⠋⠀⠀⠀⢸⣿⣇⢹⣿⣷⣰⣿⣿⠃⠀⢠⣿⣿⢃⣀⣤⣤⣾⣿⡟⠀⠀⠀⢻⣿⣆⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣿⣿⡇⠀⠀⢀⣴⣿⣿⡟⠀⣿⣿⣿⣿⠃⠀⠀⣾⣿⣿⡿⠿⠛⢛⣿⡟⠀⠀⠀⠀⠀⠻⠿⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠹⣿⣿⣶⣾⣿⣿⣿⠟⠁⠀⠸⢿⣿⠇⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠁\033[0m\n")
+                                break
 
                 
                 elif(atacar == "i" or atacar == "item" or atacar == "it"):
 
-                    print("\n itens disponiveis:\n")
-                    print("ID | Nome | Dano\n\n")
+                    print_devagar("\nItens disponiveis:\n")
+                    print_devagar("ID | Nome | Dano\n\n")
 
                     inv = Inventario()
                     inv.consultarInventarioArmasID(jogadorID)
 
-                    print("\nDigite apenas o id do item!\n")
+                    print_devagar("\nDigite apenas o id do item!\n")
                     item = input()
                     IDi = inv.getInventarioArmasID(jogadorID, item)  
 
@@ -131,52 +188,97 @@ def luta(idNPC: int, numInstancia: int, jogadorID: int):
                         danoInfligidoJogador = danoMonstro - defesaJogador
                         danoInfligidoMonstro = danoAtaque - defesaMonstro
 
-                        print(f"jogador -{danoInfligidoJogador}")
-                        print(f"monstro -{danoInfligidoMonstro}")
+                        os.system('cls')
+                        print_devagar(f"jogador -{danoInfligidoJogador}")
+                        print_devagar(f"monstro -{danoInfligidoMonstro}")
 
                         if(danoInfligidoJogador > 0):
                             novaVida = vidaJogador - danoInfligidoJogador
                             jogador.atualizarVidaPCID(jogadorID, novaVida)
-                        
+                            if(novaVida <= 0):
+                                print_um_pouco_mais_rapido("\033[0;31m   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⠀⢀⣴⣿⡶⠀⣾⣿⣿⡿⠟⠛⠁\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⣶⣶⣦⠀⠀⠀⠀⣼⣿⣿⡇⠀⣠⣿⣿⣿⠇⣸⣿⣿⣧⣤⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⢀⣴⣾⣿⡿⠿⠿⠿⠇⠀⠀⣸⣿⣿⣿⡆⠀⠀⢰⣿⣿⣿⣷⣼⣿⣿⣿⡿⢀⣿⣿⡿⠟⠛⠁⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⣴⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⢠⣿⣿⣹⣿⣿⣿⣿⣿⣿⡏⢻⣿⣿⢿⣿⣿⠃⣼⣿⣯⣤⣴⣶⣿⡤⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣼⣿⠏⠀⣀⣠⣤⣶⣾⣷⠄⣰⣿⣿⡿⠿⠻⣿⣯⣸⣿⡿⠀⠀⠀⠁⣾⣿⡏⢠⣿⣿⠿⠛⠋⠉⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣿⣿⠲⢿⣿⣿⣿⣿⡿⠋⢰⣿⣿⠋⠀⠀⠀⢻⣿⣿⣿⠇⠀⠀⠀⠀⠙⠛⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠹⢿⣷⣶⣿⣿⠿⠋⠀⠀⠈⠙⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣴⣶⣦⣤⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⡇⢰⣶⣶⣾⡿⠷⣿⣿⣿⡟⠛⣉⣿⣿⣿⠆\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⡎⣿⣿⣦⠀⠀⠀⢀⣤⣾⠟⢀⣿⣿⡟⣁⠀⠀⣸⣿⣿⣤⣾⣿⡿⠛⠁⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⣠⣾⣿⡿⠛⠉⢿⣦⠘⣿⣿⡆⠀⢠⣾⣿⠋⠀⣼⣿⣿⣿⠿⠷⢠⣿⣿⣿⠿⢻⣿⣧⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣴⣿⣿⠋⠀⠀⠀⢸⣿⣇⢹⣿⣷⣰⣿⣿⠃⠀⢠⣿⣿⢃⣀⣤⣤⣾⣿⡟⠀⠀⠀⢻⣿⣆⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣿⣿⡇⠀⠀⢀⣴⣿⣿⡟⠀⣿⣿⣿⣿⠃⠀⠀⣾⣿⣿⡿⠿⠛⢛⣿⡟⠀⠀⠀⠀⠀⠻⠿⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠹⣿⣿⣶⣾⣿⣿⣿⠟⠁⠀⠸⢿⣿⠇⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠁\033[0m\n")
+                                break
+
                         if(danoInfligidoMonstro > 0):
                             novaVidaMonstro = vidaInstacia - danoInfligidoMonstro
                             inimigoAux.atualizarVidaInstanciaID(idNPC, numInstancia, novaVidaMonstro)
                     
                     else:
+                        os.system('cls')
+                        print_devagar("\nItem inexistente\n")
+
                         danoMonstro = forca + randint(-4,3)
                         defesa = defesaJogador + randint(-4,3)
                         danoInfligidoJogador = danoMonstro - defesaJogador
 
-                        print(f"jogador -{danoInfligidoJogador}\n")
+                        print_devagar(f"jogador -{danoInfligidoJogador}\n")
 
                         if(danoInfligidoJogador > 0):
                             novaVida = vidaJogador - danoInfligidoJogador
                             jogador.atualizarVidaPCID(jogadorID, novaVida)
+                            if(novaVida <= 0):
+                                print_um_pouco_mais_rapido("\033[0;31m   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⠀⠀⠀⢀⣴⣿⡶⠀⣾⣿⣿⡿⠟⠛⠁\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⣀⣀⣄⣀⠀⠀⠀⠀⣶⣶⣦⠀⠀⠀⠀⣼⣿⣿⡇⠀⣠⣿⣿⣿⠇⣸⣿⣿⣧⣤⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⢀⣴⣾⣿⡿⠿⠿⠿⠇⠀⠀⣸⣿⣿⣿⡆⠀⠀⢰⣿⣿⣿⣷⣼⣿⣿⣿⡿⢀⣿⣿⡿⠟⠛⠁⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⣴⣿⡿⠋⠁⠀⠀⠀⠀⠀⠀⢠⣿⣿⣹⣿⣿⣿⣿⣿⣿⡏⢻⣿⣿⢿⣿⣿⠃⣼⣿⣯⣤⣴⣶⣿⡤⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣼⣿⠏⠀⣀⣠⣤⣶⣾⣷⠄⣰⣿⣿⡿⠿⠻⣿⣯⣸⣿⡿⠀⠀⠀⠁⣾⣿⡏⢠⣿⣿⠿⠛⠋⠉⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⣿⣿⠲⢿⣿⣿⣿⣿⡿⠋⢰⣿⣿⠋⠀⠀⠀⢻⣿⣿⣿⠇⠀⠀⠀⠀⠙⠛⠀⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠹⢿⣷⣶⣿⣿⠿⠋⠀⠀⠈⠙⠃⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣴⣶⣦⣤⡀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⣠⡇⢰⣶⣶⣾⡿⠷⣿⣿⣿⡟⠛⣉⣿⣿⣿⠆\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⡎⣿⣿⣦⠀⠀⠀⢀⣤⣾⠟⢀⣿⣿⡟⣁⠀⠀⣸⣿⣿⣤⣾⣿⡿⠛⠁⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⣠⣾⣿⡿⠛⠉⢿⣦⠘⣿⣿⡆⠀⢠⣾⣿⠋⠀⣼⣿⣿⣿⠿⠷⢠⣿⣿⣿⠿⢻⣿⣧⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣴⣿⣿⠋⠀⠀⠀⢸⣿⣇⢹⣿⣷⣰⣿⣿⠃⠀⢠⣿⣿⢃⣀⣤⣤⣾⣿⡟⠀⠀⠀⢻⣿⣆⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⣿⣿⡇⠀⠀⢀⣴⣿⣿⡟⠀⣿⣿⣿⣿⠃⠀⠀⣾⣿⣿⡿⠿⠛⢛⣿⡟⠀⠀⠀⠀⠀⠻⠿⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠹⣿⣿⣶⣾⣿⣿⣿⠟⠁⠀⠸⢿⣿⠇⠀⠀⠀⠛⠛⠁⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀\n")
+                                print_um_pouco_mais_rapido("\033[0;31m    ⠀⠀⠀⠀⠈⠙⠛⠛⠛⠋⠁\033[0m\n")
+                                break
 
                 elif(atacar == "poção" or atacar == "poc" or atacar == "pocao"):
-                    print("\nPoções disponiveis:\n")
-                    print("ID | Nome | Cura | Usos\n\n")
+                    print_devagar("\nPoções disponiveis:\n")
+                    print_devagar("ID | Nome | Cura | Usos\n\n")
 
                     inv2 = Inventario()
                     inv2.consultarInventarioConsumiveisID(jogadorID)
 
-                    print("\nDigite apenas o ID da poção!\n")
+                    print_devagar("\nDigite apenas o ID da poção!\n")
 
                     IdPocao = input()
                     teste = inv2.getInventarioConsumiveisID(jogadorID, IdPocao)
 
+                    os.system('cls')
                     if teste is not None:
                         novaVida = vidaJogador + teste[0][2]
-                        jogador.atualizarVidaPCID(jogadorID, novaVida)                    
+                        jogador.atualizarVidaPCID(jogadorID, novaVida)
+                    else:
+                        os.system('cls')
+                        print_devagar("\nPoção inexistente\n")
                 
+                if(vidaInstacia <= 0):
+                    print_devagar("\nVoce derrotou o monstro\n")    
+
             case 'fugir':
-                print("Você fugiu!\n")
+                print_devagar("Você fugiu!\n")
                 break
 
             case _:
-                print("\nDigite uma das opcoes!\n")
+                print_devagar("\nDigite uma das opcoes!\n")
 
-    if(vidaInstacia <= 0):
-        print("\nVoce derrotou o monstro\n")
 
-luta(3, 1, 4)
+luta(3, 2, 4)
